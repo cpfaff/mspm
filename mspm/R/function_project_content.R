1950c89890df58e817408beee6cb301e809a9512
cpfaff <ctpfaff@arcor.de>
2019-02-18 09:44:48 +0100

    Adds error checking
    * Creating a reference inside a project the function throws an
      error when the referenced content does not exist.

--- mspm/R/function_project_content.R
+++ mspm/R/function_project_content.R
@@ -6,7 +6,7 @@
 #' actual project name. This makes the project more self contained and
 #' allows references to files which do not break.
 #'
-#' @importFrom fs path
+#' @importFrom fs path dir_exists file_exists
 #'
 #' @export reference_content
 
@@ -17,5 +17,13 @@ reference_content <- function(path = NULL){
     if(is.null(path)){
         stop("the function reference_content: requires a path to the file or folder you want to reference")
     }
+
+    is_the_reference_a_file = file_exists(path(enabled_project("project_path"), "project", path))
+    is_the_reference_a_folder = dir_exists(path(enabled_project("project_path"), "project", path))
+
+    if(!all(is_the_reference_a_file, is_the_reference_a_folder)){
+        stop("the function reference_content: seems to point to nowhere the file or directory does not exist")
+    }
+
     return(path(enabled_project("project_path"), "project", path))
 }
