1f55400cfad3d6c82af3a0dffa70bfc847fa54ee
cpfaff <ctpfaff@arcor.de>
2019-02-18 09:54:54 +0100

    Removes the check for existence
    * This otherwise limits the capability of usage. When I want to
      reference a file for creation then it will not work e.g. during
      writing a csv file.

--- mspm/R/function_project_content.R
+++ mspm/R/function_project_content.R
@@ -17,13 +17,5 @@ reference_content <- function(path = NULL){
     if(is.null(path)){
         stop("the function reference_content: requires a path to the file or folder you want to reference")
     }
-
-    is_the_reference_a_file = file_exists(path(enabled_project("project_path"), "project", path))
-    is_the_reference_a_folder = dir_exists(path(enabled_project("project_path"), "project", path))
-
-    if(!all(is_the_reference_a_file, is_the_reference_a_folder)){
-        stop("the function reference_content: seems to point to nowhere the file or directory does not exist")
-    }
-
     return(path(enabled_project("project_path"), "project", path))
 }
