820e62d45abb8b4b83b5fd29f64621c71965fe78
cpfaff <ctpfaff@arcor.de>
2019-02-26 11:43:50 +0100

    Remove bracket

--- yspm/R/function_enable_project.R
+++ yspm/R/function_enable_project.R
@@ -22,13 +22,13 @@
 #'     project_category = "Phd"
 #'   )
 #' )
-#'
+#' 
 #' enable_project(
 #'   root_folder = "~/",
 #'   project_name = "my_phd_project"
 #' )
 #' }
-#'
+#' 
 #' @importFrom checkpoint checkpoint
 #' @importFrom fs path file_create is_file dir_create dir_delete is_dir dir_exists file_exists
 #' @export enable_project
@@ -121,9 +121,9 @@ enable_project <- function(root_folder = getwd(), project_name = NULL, project_p
     message("")
 
     loaded_packages <- names(sessionInfo()$otherPkgs)
-      quiet(suppressWarnings(lapply(detachable_packages, function(package) { try(detach(paste0("package:", package), character.only = TRUE, unload = TRUE, force = TRUE), silent = T) })))
-    }
-
+    quiet(suppressWarnings(lapply(detachable_packages, function(package) {
+      try(detach(paste0("package:", package), character.only = TRUE, unload = TRUE, force = TRUE), silent = T)
+    })))
   }
 
   require(yspm)
