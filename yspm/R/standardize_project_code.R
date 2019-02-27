2e49f2283db27bcd7d716edcfe81712eb6e4ed84
cpfaff <ctpfaff@arcor.de>
2019-02-27 08:28:25 +0100

    Small renaming and improvements in documentation
    * The style function now also works without a project
      enabled. However it defaults to the enabled project
    * Improves documentation and renames parameter for more
      consistency

---
+++ yspm/R/standardize_project_code.R
@@ -0,0 +1,23 @@
+#' Clean up the source code of your project
+#'
+#' A small wrapper to the styler package which allows you easily standarize
+#' the source code of your whole project using the tidyverse style guide
+#' to improve code readability. The corrections include spacing, indentation,
+#' the use of the right assignment operator... etc.
+#'
+#' @param project_path Is the full path to the folder in which the active
+#'        project resides in.
+#'
+#' @examples
+#' \dontrun{
+#' enable_project(...)
+#'
+#' style_project()
+#' }
+#'
+#' @importFrom styler style_dir
+#' @export standardize_project_code
+
+standardize_project_code <- function(project_path = yspm::enabled_project("project_path")) {
+  suppressWarnings(style_dir(path))
+}
