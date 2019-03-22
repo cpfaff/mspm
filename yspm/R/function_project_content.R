1af29b32189646f6a19e1aad2f756c78f77f55f8
cpfaff <ctpfaff@arcor.de>
2019-03-22 08:16:54 +0100

    Adds modularity functions
    * This however is a stub at the moment. It will help in the future
      to define projects in the main management file.

--- yspm/R/function_project_content.R
+++ yspm/R/function_project_content.R
@@ -13,7 +13,7 @@
 #' data
 #' ...
 #' ...
-#' }
+#'}
 #'
 #' @importFrom fs path
 #'
---
+++ yspm/R/function_use_that.R
@@ -0,0 +1,16 @@
+#' A function that allows building modular projects
+#'
+#' Each project is different and everybody has a different taste and needs of
+#' packages. Thus the yspm is modular allowing to script each project to your
+#' needs. It here uses a similar approach to the the use_this package. Calling
+#' the function will add packages and content to your active project to enable
+#' certain functionality.
+#'
+
+#' @export use_that
+use_that <- function(feature) {
+  if ("drake" %in% feature) {
+    print("adding drake package to your packages")
+    print("install the drake package")
+  }
+}
