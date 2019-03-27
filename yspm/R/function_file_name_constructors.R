f56033f92ea9af21390dfa29e3c47637314cdbfb
cpfaff <ctpfaff@arcor.de>
2019-03-27 10:04:24 +0100

    Add generic function to create file names
    * This now allows to granularly create and use consistent
      file names in your project (e.g. for writing and reading
      purposes). This closes #21

--- yspm/R/function_file_name_constructors.R
+++ yspm/R/function_file_name_constructors.R
@@ -1,3 +1,41 @@
+#' Create file names for your project
+#'
+#' The package comes with family of functions which will help you with
+#' referencing and naming content inside of a project. This provides you 
+#' with an unobtrusive way to keep your project clean, while adhering to 
+#' a unified naming schema. 
+#'
+#' @param name A filename to create for a plot. It will be forced to snake
+#'        case.
+#' @param extension The extension the file is using. This string will be 
+#'        forced to lower case letters only and appended to the file 
+#'        name.
+#'
+#' @return The function returns the constructed file name
+#'
+#' @examples
+#' \dontrun{
+#'  compile_filename(name = "01 my first plot", extension = "PNG")
+#' }
+#'
+#' @importFrom snakecase to_snake_case
+#' @export compile_filename 
+
+compile_filename <- function(name = NULL, extension = NULL) {
+  the_function = match.call()[[1]]
+  check_if_project_is_enabled(the_function)
+
+  # error checking
+  if (missing(name)) {
+    stop(paste(the_function, ": requires the argument name"))
+  }
+  if (missing(extension)) {
+    stop(paste(the_function, ": requires the argument extension"))
+  }
+
+  return(paste(c(as.character(snakecase::to_snake_case(name)), as.character(tolower(extension))), collapse = "."))
+}
+
 #' Create a file name for a plot
 #'
 #' The package comes with family of functions which will help you with
