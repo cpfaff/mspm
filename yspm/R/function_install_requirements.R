df5599ba99d48c1f36fc5346a1bd5302b7b03225
cpfaff <ctpfaff@arcor.de>
2019-04-05 08:40:40 +0200

    Separates installing and listing requirements

--- yspm/R/function_install_requirements.R
+++ yspm/R/function_install_requirements.R
@@ -5,21 +5,21 @@
 #' a project to ensure all requirements are installed. The function detects
 #' all calls to library(), require(), import::from().
 #'
-#' @param root_path The path to the project.
-#' @param path Provide the path folder all scripts are located in.
+#' @param search_path Provide the path to a folder all your scripts are located 
+#'        in. This can be e.g. the root directory of a project. The function 
+#'        searches recursively all folders for R and R-Markdown files.
 #'
 #' @importFrom stringr str_extract_all
 #' @export list_requirements
 #
-list_requirements <- function(root_path, path = yspm::project_structure("folder_source")) {
-
-  path <- suppressWarnings(normalizePath(path(root_path, path)))
+list_requirements <- function(search_path = getwd()) {
+  search_path <- suppressWarnings(normalizePath(path(search_path)))
 
   list_of_files <-
-    list.files(path = path, recursive = T, ignore.case = T, pattern = "*.(r|rmd)$")
+    list.files(path = search_path, recursive = T, ignore.case = T, pattern = "*.(r|rmd)$")
 
   list_of_files_with_path <-
-    lapply(list_of_files, function(file_name) path(path, file_name))
+    lapply(list_of_files, function(file_name) path(search_path, file_name))
 
   if (length(list_of_files) > 0) {
     big_message("List requirements")
@@ -45,40 +45,38 @@ list_requirements <- function(root_path, path = yspm::project_structure("folder_
   }
 }
 
-# A function to install packages which are not yet installed
+#' A function to install packages which are not yet installed 
+#'
+#' You can call the function to install all packages you pass in the names of
+#' which are not yet installed in any of your active library paths. Is is used
+#' together with list_requirements when a project is enabled to ensure that all 
+#' the required dependencies are installed.  
+#'
+#' @param packages a vector or list providing all package names to be installed.
+#'
+#' @export install_requirements
 #
-# You can call the function to install all packages in which are not yet
-# installed in any of your active library paths. Is is used when a project
-# is enabled to ensure that all requirements are installed.  
-#
-# @param path Provide the path folder all scripts are located in.
-#
-# @export install_requirements
-#
-# install_requirements <- function(path = yspm::project_structure("folder_source")) {
-  # path <- suppressWarnings(normalizePath(path(path)))
+install_requirements <- function(package_names) {
+  the_function <- match.call()[[1]]
+  check_if_project_is_enabled(the_function)
 
-# path = yspm::project_structure("folder_source")
-  # path <- suppressWarnings(normalizePath(path(path)))
-  # the_function <- match.call()[[1]]
-  # check_if_project_is_enabled(the_function)
+  local_packages <- rownames(installed.packages())
+  package_count <- length(setdiff(packages, local_packages))
 
-    # local_packages <- rownames(installed.packages())
-    # package_count <- length(setdiff(required_packages, local_packages))
+    if (package_count > 0) {
+      if (!all(packages %in% local_packages)) {
+        small_message("Installing")
+        message("")
+        message(paste("* package count:", package_count))
+        message("---")
+        message("")
 
-    # if (package_count > 0) {
-      # if (!all(required_packages %in% local_packages)) {
-        # small_message("Installing")
-        # message("")
-        # message(paste("* package count:", package_count))
-        # message("---")
-        # message("")
+        for (package in setdiff(packages, local_packages)) {
+          message(paste("* package:", package))
+        }
 
-        # for (package in setdiff(required_packages, local_packages)) {
-          # message(paste("* package:", package))
-        # }
-
-        # install.packages(unlist(setdiff(required_packages, local_packages)))
-      # }
-    # }
+        install.packages(unlist(setdiff(packages, local_packages)))
+      }
+    }
+}
 
